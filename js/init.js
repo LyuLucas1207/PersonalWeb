c9b838cf7b13815133bf1c460cce90166448a762
LyuLucas1207 <lyuchongkai@gmail.com>
2024-07-17 00:44:42 +0800

    2024-07-16-00-44

--- js/init.js
+++ js/init.js
@@ -3,7 +3,7 @@ import * as tools from './tools.js';
 document.addEventListener('DOMContentLoaded', async function() {
     tools.initializeLoader(20, 30,20, 30);
     //(selector, selector_cover = null, greeting = null, accordingToTime = false, showingTime = 3)
-    tools.avatarExpand('.avatar', '.avatar_cover', "亲爱的吕先生，欢迎您！", true, 10);
+    tools.avatarExpand('.avatar', '.avatar_cover', "亲爱的吕先生，欢迎您！", true, 30);
     tools.randomImgGenerator(1, 12, 1, 2, 'body','./img/Background/background');
     tools.randomImgGenerator(1, 12, 1, 2, '.avatar_img','./img/Avatar/avatar');
     tools.controlStar(25, 100, '.section-banner');
