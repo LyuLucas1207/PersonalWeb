e8fd01f926efce77ba6ecfe2fc0e75aa7ef3b829
LyuLucas1207 <lyuchongkai@gmail.com>
2024-07-24 17:53:39 +0800

    2024-07-24-17-53

--- js/init.js
+++ js/init.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', async function () {
     //generateElements.generateElements('backgroundColor', '.center_inner_content_container', false, false, null, null, null, 0, 255, 0, 1);
     tools.avatarExpand('.avatar', '.avatar_cover', "尊敬的访客！", true, 30);
     tools.randomImgGenerator(1, 13, 1, 2, 'body', './img/Background/background', 'jpg');
-    tools.randomImgGenerator(1, 7, 1, 2, '.avatar_img', './img/Avatar/avatar', 'jpg');
+    tools.randomImgGenerator(1, 30, 1, 2, '.avatar_img', './img/Avatar/avatar', 'jpg');
     Elements.generateElements('star', '.section-banner', 25, 100);
     Elements.generateElements('timeCard', '.time_card');
     const minLoadTime = tools.minLoadTime(1); // 5 seconds minimum load time
