cdafdd201fb16b74da2368ee922459a68ad6ec4d
LyuLucas1207 <lyuchongkai@gmail.com>
2024-07-25 18:35:22 +0800

    2024-07-25-18-35

--- js/init.js
+++ js/init.js
@@ -1,18 +1,31 @@
 import * as tools from './tools.js';
 import Elements from './generates.js';
 
+// Generate various elements and functionalities
+const fn1 = () => Elements.generateElements('rain', '.loader', 20, 30, 20, 30);
+const fn2 = () => Elements.generateElements('favicon', './img/Icon/icon', 'image/png');
+const fn3 = () => Elements.generateElements('star', '.section-banner', 25, 100);
+const fn4 = () => Elements.generateElements('timeCard', '.time_card');
+const fn5 = () => Elements.generateElements('backgroundColor', '.center_inner_content_container', true, false, 255, 255, 255, null, null, null, 0.1, 0.6);
+
+function wrappedFunctions(...fns) {
+    return fns;
+}
+
+// Function to add tasks to the microtask queue and wait for all to complete
+function addMicrotask(fns) {
+    return Promise.all(fns.map(fn => fn()));
+}
+
 document.addEventListener('DOMContentLoaded', async function () {
-    Elements.generateElements('rain', '.loader', 20, 30, 20, 30);
-    Elements.generateElements('favicon', './img/Icon/icon', 'image/png');
-    Elements.generateElements('star', '.section-banner', 25, 100);
-    Elements.generateElements('timeCard', '.time_card');
-    Elements.generateElements('backgroundColor', '.center_inner_content_container', true, false, 255, 255, 255, null, null, null, 0.1, 0.6);
-    //generateElements.generateElements('backgroundColor', '.center_inner_content_container', false, false, null, null, null, 0, 255, 0, 1);
+    const fnList = wrappedFunctions(fn1, fn2, fn3, fn4, fn5);
+    // Execute all function definitions once DOM is fully loaded，await will wait for all tasks to complete
+    await addMicrotask(fnList);
+
     tools.avatarExpand('.avatar', '.avatar_cover', "尊敬的访客！", true, 30);
    
-    const minLoadTime = tools.minLoadTime(1); // 5 seconds minimum load time
+    const minLoadTime = tools.minLoadTime(1); // Set minimum load time
     const resourcesLoaded = tools.checkResourcesLoaded();
-    await Promise.all([minLoadTime, resourcesLoaded]);
-    tools.showContent('.loader', 3);
+    await Promise.all([minLoadTime, resourcesLoaded]); // Ensure all resources are loaded
+    tools.showContent('.loader', 3); // Show content after loading
 });
-
